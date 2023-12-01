b41b425527098e399b815bcbbb985617fea586df
rishi <rishirajbabu123@gmail.com>
2023-12-01 12:06:21 +0530

    updated

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -3,8 +3,22 @@ function buttonclick(e){
     document.getElementById('text').value+= e;
 }
 function clr(){
-document.getElementById('clear').value= " ";
-console.log()
+document.getElementById('text').value= " ";
+}
+function equalTo(){
+   document.getElementById('text').value
+
 }
 
-
+function plus(a){
+    document.getElementById('text').value+= a
+}
+function minus(a){
+    document.getElementById('text').value+= a
+}
+function multiply(a){
+    document.getElementById('text').value+= a
+}
+function divide(a){
+    document.getElementById('text').value+= a
+}
