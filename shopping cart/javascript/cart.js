3cb5309ff99e6019ce5f3bced23833ad6462a10d
rishi <rishirajbabu123@gmail.com>
2023-12-14 12:58:11 +0530

    updated

--- shopping cart/javascript/cart.js
+++ shopping cart/javascript/cart.js
@@ -1,39 +1,56 @@
 
-function minus(id,price){//passing unique id as referance to this function
+function minus(id, price) {//passing unique id as referance to this function
     // console.log(document.getElementById(price))
-    let value= document.getElementById(price)
-    let temprice =value.textContent;
+    let value = document.getElementById(price)
+    let temprice = value.textContent;
     console.log(temprice)
-    let tempnum= document.getElementById(id).value //we are fetching the value inside a particular id
- 
+    let tempnum = document.getElementById(id).value //we are fetching the value inside a particular id
+
     tempnum--;
+    // alert(temprice)
+
+    if (tempnum < 1) {
+        tempnum = 1;
+    }
+    displayValue(id, tempnum);
+}
+
+// let temp = 79999;
+function plus(id, price) {
+
+    //  alert(price)
+    let value = document.getElementById(price)
+
+    let tempnum = document.getElementById(id).value
+    // let temprice= document.getElementById('updatePrice').getAttribute('value')
+    let temprice = value.textContent;
+    //  console.log(temprice)
+    let temp= temprice;
     
-    if(tempnum<1){
-        tempnum=1;
+    if(tempnum >= 1) {
+        
+        temprice = temp * 2;
+        temp = temp * 2;
+       
+          //console.log(temprice)
+       
     }
-    displayValue(id,tempnum);
-}
-
-function plus(id,price){
-    alert(price)
-    let tempnum= document.getElementById(id).value 
-    let temprice= document.getElementById('updatePrice').getAttribute('value')
-    // console.log(temprice)
+    
     tempnum++;
-
-    temprice= temprice*2;
-     console.log(temprice)
-        if(tempnum>5){
-        tempnum=5;
+    
+    if (tempnum > 5) {
+        tempnum = 5;
     }
-    displayValue(id,tempnum,temprice);
+    console.log(temprice)
+    displayValue(id, tempnum, temprice);
+
 }
 
 
-function displayValue(id,tempnum,temprice){
+
+function displayValue(id, tempnum, temprice) {
     document.getElementById(id).value = tempnum;
-    //document.getElementById('updatePrice').value= temprice;
-    document.getElementById('updatingPrice').getAttribute(id)= temprice;
+    document.getElementById('updatingPrice').textContent = temprice;
 
 }
 
