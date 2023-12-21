e2eddcb281a9dcfe2126f5b227b9bb65602b19b9
rishi <rishirajbabu123@gmail.com>
2023-12-21 23:05:14 +0530

    updated

--- shopping cart dynamic/javascript/dynamiccart.js
+++ shopping cart dynamic/javascript/dynamiccart.js
@@ -1,91 +1,66 @@
-if (typeof products === 'undefined') {
-    // Initialize products only if it's not defined
-    var products = [
-        {
-            "product": "Samsung Galaxy S23",
-            "image": "../img/s23.jpg",
-            "price": {
-                "symbol": "₹",
-                "basePrice": 2,
-                "updatePrice": 0
-            }
-        },
-        {
-            "product": "Bose QuietComfort Ultra earbuds",
-            "image": "../img/earbuds.jpg",
-            "price": {
-                "symbol": "₹",
-                "basePrice": 5,
-                "updatePrice": 0
-            }
-        },
-        {
-            "product": "Galaxy Watch5 Bluetooth (40mm)",
-            "image": "../img/watch.jpg",
-            "price": {
-                "symbol": "₹",
-                "basePrice": 3,
-                "updatePrice": 0
-            }
+// create products to dynamicaly use, by creating  array of product details
+const products = [
+    {
+        "product": "Samsung Galaxy S23",
+        "image": "../img/s23.jpg",
+        "price": {
+            "symbol": "₹",
+            "basePrice": 2,
+            "updatePrice": 0
         }
-    ];
-}
+    },
+    {
+        "product": "Bose QuietComfort Ultra earbuds",
+        "image": "../img/earbuds.jpg",
+        "price": {
+            "symbol": "₹",
+            "basePrice": 5,
+            "updatePrice": 0
+        }
+    },
+    {
+        "product": "Galaxy Watch5 Bluetooth (40mm)",
+        "image": "../img/watch.jpg",
+        "price": {
+            "symbol": "₹",
+            "basePrice": 3,
+            "updatePrice": 0
+        }
+    }
+];
+
 
 document.addEventListener('DOMContentLoaded', function () {
-    function processProductData(products) {
-        function generateProductHTML(product) {
+    function processProductData(products) { //this function is used to process the product data given in the array of products
+        function generateProductHTML(product) {//this function used to generate the dynamically created array of products
             return `
-                <div class="product-desc-box">
-                    <div class="imgborder">
-                        <img src="${product.image}" alt="${product.product}">
-                    </div>
-                    <div>
-                        <div>
-                            <div class="symbol">${product.price.symbol}
-                                <div class="updatePrice" data-price="${product.price.basePrice}">${product.price.basePrice}</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="product-price-box">
-                    <div class="buttongroup">
-                        <button class="minus" onclick="updateValue('${product.product}', -1)"><b>-</b></button>
-                        <input class="textbox" id="${product.product}" type="text" readonly>
-                        <button class="plus" onclick="updateValue('${product.product}', 1)"><b>+</b></button
-                    </div>
-                    <div>
-                        <div class="symbol">${product.price.symbol}
-                            <div class="updatingprice" data-updatingPriceId="${product.product}">0</div>
-                        </div>
-                    </div>
-                </div>`;
-            return `       
             <div class="product-desc-box">
-                <div class="imgborder">
-                 <img src="${product.image}" alt="${product.product}">
-                </div>
-                <div>
-                <div> Samsung Galaxy S23
-                    <div class="symbol" >&#8377;
-            <div id="updatePrice"> 2</div>
+            <div class="wrap">
+            <div class="imgborder">
+                <img src="${product.image}" alt="${product.product}">
+            </div>
+            <div class="product-info">
+                <div class="product-name">${product.product}</div>
+                <div class="product-price">
+                    <div class="symbol">${product.price.symbol}
+                        <div class="updatePrice" data-price="${product.price.basePrice}">${product.price.basePrice}</div>
                     </div>
-        </div>
-        </div>
-        </div>
-
+                </div>
+                </div>
+            </div>
             <div class="product-price-box">
                 <div class="buttongroup">
-                    <button id="minus" onclick="minus('textboxOne','updatePrice','updatingPrice')"><b>-</b></button>
-                    <input class="textbox" id="textboxOne" type="text" readonly>
-                    <button id="plus" onclick="plus('textboxOne','updatePrice','updatingPrice')"><b>+</b></button>
+                    <button class="minus" onclick="updateValue('${product.product}', -1)"><b>-</b></button>
+                    <input class="textbox" id="${product.product}" type="text" readonly>
+                    <button class="plus" onclick="updateValue('${product.product}', 1)"><b>+</b></button>
                 </div>
                 <div>
-                    <div class="symbol"> &#8377;
-                    <div class="updatingprice" id="updatingPrice"> </div>
+                    <div class="symbol">${product.price.symbol}
+                        <div class="updatingprice" data-updatingPriceId="${product.product}" data-price="${product.price.basePrice}">0</div>
+                    </div>
                 </div>
-                </div>
-            </div>`
-
+            </div>
+        </div>`;
 
         }
 
@@ -108,8 +83,14 @@ document.addEventListener('DOMContentLoaded', function () {
 function updateValue(id, change) {
     let valueElement = document.querySelector(`[data-updatingPriceId="${id}"]`);
     let tempnum = parseInt(document.getElementById(`${id}`).value, 10) || 0;
-    let temprice = parseFloat(valueElement.getAttribute('data-price')) || 0;
-    console.log(valueElement,tempnum,temprice)
+
+    // Use Number() instead of Number.parseFloat() for simplicity
+    let temprice = Number(valueElement.getAttribute('data-price'));
+
+    // Check if temprice is NaN or not a number
+    temprice = isNaN(temprice) ? 0 : temprice;
+
+    console.log(`Before update - tempnum: ${tempnum}, temprice: ${temprice}`);
 
     tempnum += change;
 
@@ -118,8 +99,9 @@ function updateValue(id, change) {
     } else if (tempnum > 5) {
         tempnum = 5;
     }
-
-    temprice = tempnum * parseFloat(valueElement.getAttribute('data-price'));
+    console.log(`Value element:`, valueElement);
+    console.log(`data-price attribute:`, valueElement.getAttribute('data-price'));
+    temprice = tempnum * temprice;
     document.getElementById(`${id}`).value = tempnum;
     valueElement.textContent = temprice;
 
@@ -127,13 +109,14 @@ function updateValue(id, change) {
     document.getElementById('totalPrice').textContent = totalPrice;
 }
 
+
 function total(productIds) {
     let totalPrice = 0;
 
     for (let i = 0; i < productIds.length; i++) {
         let productId = productIds[i];
         let updatingPriceElement = document.querySelector(`[data-updatingPriceId="${productId}"]`);
-        let updatingPrice = parseFloat(updatingPriceElement.getAttribute('data-price')) || 0;
+        let updatingPrice = parseFloat(updatingPriceElement.textContent) || 0;
 
         if (!isNaN(updatingPrice)) {
             console.log(`Updating Price for ${productId}:`, updatingPrice);
